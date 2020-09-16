b32350251eefd441f41dcac0a254a5454a22b002
don <don@MacBook-Pro.local>
2020-09-16 23:46:16 +0300

    fix initialization of settings.json and error handling

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import * as vscode from "vscode";
 import * as jsonSettings from "./data/settingsCollection.json";
 
 export const checkAndCreateSettings = (
@@ -10,36 +11,34 @@ export const checkAndCreateSettings = (
   try {
     if (!fs.existsSync(fullPath)) {
       fs.mkdirSync(fileDir, { recursive: true });
+      // Create empty settings.json to start with
       checkAndCreateFile(fullPath);
-    } else {
-      const currentContent: string = fs.readFileSync(fullPath, "utf-8");
-      console.log(jsonSettings);
-      const settings: object = !currentContent
-        ? {}
-        : JSON.parse(currentContent);
-
-      let contentForWrite = {
-        ...settings,
-      };
-      switch (lang) {
-        case "javascript":
-          contentForWrite = {
-            ...jsonSettings.javascript,
-          };
-          break;
-        case "python":
-          contentForWrite = {
-            ...jsonSettings.python,
-          };
-          break;
-        default:
-          break;
-      }
-
-      checkAndCreateFile(fullPath, JSON.stringify(contentForWrite));
     }
+    const currentContent: string = fs.readFileSync(fullPath, "utf-8");
+
+    const settings: object = !currentContent ? {} : JSON.parse(currentContent);
+
+    let contentForWrite = {
+      ...settings,
+    };
+    switch (lang) {
+      case "javascript":
+        contentForWrite = {
+          ...jsonSettings.javascript,
+        };
+        break;
+      case "python":
+        contentForWrite = {
+          ...jsonSettings.python,
+        };
+        break;
+      default:
+        break;
+    }
+
+    checkAndCreateFile(fullPath, JSON.stringify(contentForWrite));
   } catch (err) {
-    console.log(err);
+    vscode.window.showErrorMessage(err.message);
   }
 };
 
@@ -47,6 +46,6 @@ const checkAndCreateFile = (fileName: string, content: string = ""): void => {
   try {
     fs.writeFileSync(fileName, content);
   } catch (err) {
-    console.log(err);
+    vscode.window.showErrorMessage(err.message);
   }
 };
